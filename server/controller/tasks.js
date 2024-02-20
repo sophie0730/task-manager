2fed81a4ba595ce0e397300edfb65f5cef7937e4
Sophie <sophie010017@gmail.com>
2024-02-20 11:48:07 +0800

    feat: complete tasks CRUD

--- server/controller/tasks.js
+++ server/controller/tasks.js
@@ -1,19 +1,64 @@
-export const getAllTasks = (req, res) => {
-  res.send('get all tasks');
+import Task from "../model/tasks.js";
+
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 }
 
-export const addTasks = (req, res) => {
-  res.json(req.body);
+export const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });   
+  } catch(error) {
+    res.status(500).json({ msg: error });
+  }
 }
 
-export const getTask = (req, res) => {
-  res.send('get one tasks');
+export const getTask = async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const task = await Task.findById({ _id: taskId });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskId}`});
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error});
+  }
 }
 
-export const updateTask = (req, res) => {
-  res.send('update task');
+export const updateTask = async(req, res) => {
+  try {
+    const {id:taskId} = req.params;
+    const task = await Task.findOneAndUpdate({ _id:taskId}, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskId}`});
+    }
+    
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error});
+  }
+
 }
 
-export const deleteTask = (req, res) => {
-  res.send('delete task');
+export const deleteTask = async (req, res) => {
+  const {id:taskId} = req.params;
+  const task = await Task.findOneAndDelete({_id:taskId});
+
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id: ${taskId}`});
+  }
+  res.status(200).json({ task: null, status: 'success'});
 }
